234d727ac97f54c3e38f9c3e3a0ca5bbd02f858f
MhammdAli <101601796+MhammdAli@users.noreply.github.com>
2025-06-28 23:00:08 +0300

    Update __manifest__.py

--- whatsapp_connector/__manifest__.py
+++ whatsapp_connector/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Whatsapp Connector',
-    'version': '18.0',
+    'version': '18.0.1.0.0',
     'category': 'services/whatsapp',
     'summary': 'Whatsapp Connector App for Odoo',
     'description': """
